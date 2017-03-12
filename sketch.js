bd331985d553398a6a3196893a5d625fb6c93d5d
Dominik Tomczak <dominik.tomczak@netguru.pl>
2017-03-12 21:05:18 +0100

    add sliders and buttons and handle basic actions

--- sketch.js
+++ sketch.js
@@ -1,3 +1,53 @@
 function preload() {
   song = loadSound('assets/test_audio.mp3');
 }
+
+function setup() {
+  createCanvas(350, 400);
+
+  sliderPitch = createSlider(0, 100, 40);
+  sliderPitch.position(100, 200)
+  sliderPitchLabel = createElement('label', 'pitch');
+  sliderPitchLabel.position(50, 200)
+
+  sliderVolume = createSlider(0, 100, 50);
+  sliderVolume.position(100, 250)
+  sliderVolumeLabel = createElement('label', 'volume');
+  sliderVolumeLabel.position(50, 250)
+
+  buttonPlay = createButton('▶');
+  buttonPlay.position(100, 300)
+  buttonPlay.mousePressed(playSong);
+
+  buttonStop = createButton('▩');
+  buttonStop.position(150, 300);
+  buttonStop.mousePressed(stopSong);
+
+  buttonPause = createButton('❚❚');
+  buttonPause.position(200, 300);
+  buttonPause.mousePressed(pauseSong);
+}
+
+function draw() {
+  background(127);
+
+  var speed = map(sliderPitch.value(), 0, 100, 0.9, 1.1);
+  song.rate(speed);
+
+  var volume = map(sliderVolume.value(), 0, 100, 0, 1);
+  song.setVolume(volume);
+}
+
+function playSong() {
+  if (!song.isPlaying()) {
+    song.play();
+  }
+}
+
+function stopSong() {
+  song.stop();
+}
+
+function pauseSong() {
+  song.pause();
+}
